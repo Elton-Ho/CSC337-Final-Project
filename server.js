2a1e3d12c223fa17850009ef7b2c6990204e575c
Elton-Ho <eltonho77@gmail.com>
2025-04-21 11:24:23 -0700

    Created the start of the server

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require("express")
+var app = express()
+var path = require("path")
+
+app.get("/", function(req,res){ // homepage
+
+})
+
+app.listen(8080, function(){
+    console.log("Server running localhost:8080/")
+})
