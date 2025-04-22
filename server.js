3dc76942039db69081bbe500ed0e813c659fc8ac
Elton-Ho <eltonho77@gmail.com>
2025-04-21 17:47:43 -0700

    Created the HTML for the resume-form with adding sections functionality

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ app.get("/", function(req,res){ // homepage
 
 })
 
+app.get("resume_form", function(req,res){ // homepage
+    
+})
+
+
 app.listen(8080, function(){
     console.log("Server running localhost:8080/")
 })
