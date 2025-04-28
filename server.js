d919d894ad33463316183e44a65467bbcc51a3f1
Elton-Ho <eltonho77@gmail.com>
2025-04-27 22:59:47 -0700

    Added remove item feature

--- server.js
+++ server.js
@@ -6,10 +6,13 @@ app.get("/", function(req,res){ // homepage
 
 })
 
-app.get("resume_form", function(req,res){ // homepage
-    
+app.get("/resume_form", function(req,res){
+    res.sendFile(path.join(__dirname, "resume-form.html"))
 })
 
+app.get("/print", function(req,res){
+    res.send(req.query)
+})
 
 app.listen(8080, function(){
     console.log("Server running localhost:8080/")
