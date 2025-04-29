5d051f3ffe4a29dbcdcc582782891e4aad7e4f12
Elton-Ho <eltonho77@gmail.com>
2025-04-28 20:49:41 -0700

    Added the display resume html and moved files into the public

--- server.js
+++ server.js
@@ -55,11 +55,34 @@ app.get('/src.js', function (req, res){
 })
 
 app.get("/resume_form", function(req,res){
-    res.sendFile(path.join(__dirname, "resume-form.html"))
+    res.sendFile(path.join(rootFolder, "resume-form.html"))
 })
 
-app.get("/print", function(req,res){
-    res.send(req.query)
+app.post("/print", express.urlencoded({'extended':true}), function(req,res){
+    var html = `<!DOCTYPE html><html><body><div style="width:80%; margin-left:10%; margin-right:10%; position:absolute;">`
+    for (item of Object.keys(req.body)){
+        console.log(req.body[item])
+        if (req.body[item]){
+            if (item == "name") html += `<h2 style = 'text-align: center; margin: 0px; font-size: 19px;''>${req.body[item]}</h2>`
+            if (item == "address" || item == "email" || item == "phone") html += `<p style = 'text-align: center; margin: 0px; font-size: 14px;'>${req.body[item]}</p>`
+            if (item.includes("section-title"))  html += `<h3 style = 'margin-bottom: 0px; font-size: 17px;'>${req.body[item]}</h3>
+            <hr style = 'margin: 0px; border:black 1px solid;'></hr>`
+            if (item.includes("item-name")) html += `<span style ='font-weight: bold; margin: 0px; font-size: 15px;'>${req.body[item]}</span>`
+            if (item.includes("date-start")) html += `<span style ='position: absolute; right: 0px; margin: 0px; font-size: 15px;font-style: italic;'>${req.body[item]}`
+            if (item.includes("date-end")) html += `- ${req.body[item]}</span>` 
+            if (item.includes("role")) html += `<p style ='margin: 0px; font-size: 15px;font-style: italic;'>${req.body[item]}</p>`        
+            if (item.includes("description")){
+                html += "<ul style = 'list-style-position: outside; margin: 0px;'>"
+                for (var row of req.body[item].split("\n")){
+                    html += `<li style ='margin: 0px; font-size: 15px;font-style;'>${row}</li>` 
+                }
+                html += "</ul>"
+            }
+        }  
+    }
+    html += `</div></body></html>`
+
+    res.send(html)
 })
 
 app.listen(8080, function(){
