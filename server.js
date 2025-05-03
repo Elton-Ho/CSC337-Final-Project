a6f6d1e79909f88f472965ac5c477767fedf0615
Elton-Ho <eltonho77@gmail.com>
2025-05-02 17:40:34 -0700

    Implemented database into resumes

--- server.js
+++ server.js
@@ -58,31 +58,51 @@ app.get("/resume_form", function(req,res){
     res.sendFile(path.join(rootFolder, "resume-form.html"))
 })
 
-app.post("/print", express.urlencoded({'extended':true}), function(req,res){
-    var html = `<!DOCTYPE html><html><body><div style="width:80%; margin-left:10%; margin-right:10%; position:absolute;">`
-    for (item of Object.keys(req.body)){
-        console.log(req.body[item])
-        if (req.body[item]){
-            if (item == "name") html += `<h2 style = 'text-align: center; margin: 0px; font-size: 19px;''>${req.body[item]}</h2>`
-            if (item == "address" || item == "email" || item == "phone") html += `<p style = 'text-align: center; margin: 0px; font-size: 14px;'>${req.body[item]}</p>`
-            if (item.includes("section-title"))  html += `<h3 style = 'margin-bottom: 0px; font-size: 17px;'>${req.body[item]}</h3>
-            <hr style = 'margin: 0px; border:black 1px solid;'></hr>`
-            if (item.includes("item-name")) html += `<span style ='font-weight: bold; margin: 0px; font-size: 15px;'>${req.body[item]}</span>`
-            if (item.includes("date-start")) html += `<span style ='position: absolute; right: 0px; margin: 0px; font-size: 15px;font-style: italic;'>${req.body[item]}`
-            if (item.includes("date-end")) html += `- ${req.body[item]}</span>` 
-            if (item.includes("role")) html += `<p style ='margin: 0px; font-size: 15px;font-style: italic;'>${req.body[item]}</p>`        
-            if (item.includes("description")){
-                html += "<ul style = 'list-style-position: outside; margin: 0px;'>"
-                for (var row of req.body[item].split("\n")){
-                    html += `<li style ='margin: 0px; font-size: 15px;font-style;'>${row}</li>` 
-                }
-                html += "</ul>"
-            }
-        }  
-    }
-    html += `</div></body></html>`
+app.get("/own_resume/:username", function(req,res){
+    client.connect()
+    .then(function(){
+        var db = client.db(dbName)
+        var resumes = db.collection("resumes")
+        console.log(req.params.username)
+        return resumes.findOne({username: req.params.username})
+    })
+    .then(function(found){
+        console.log(found)
+        var html = utils.getResumeHtml(found)
+        res.send(html)
+    })
+    .catch(function(err){
+        console.log(err)
+    })
+    .finally(function(){
+        client.close()
+    })
+    
+})
 
-    res.send(html)
+app.post("/resume_action", express.urlencoded({ extended: true }), function(req,res){
+    client.connect()
+    .then(async function(){
+        var db = client.db(dbName)
+        var resumesCollection = db.collection("resumes")
+        var resume = await resumesCollection.findOne({username: req.body.username})
+        console.log(resume)
+        if (resume) 
+            return resumesCollection.updateOne({username: req.body.username}, {$set:req.body})
+        else
+            return resumesCollection.insertOne(req.body)
+    })
+    .then(function(){
+        var html = utils.getResumeHtml(req.body)
+        res.send(html)
+    })
+    .catch(function(err){
+        console.log(err)
+    })
+    .finally(function(){
+        client.close()
+    })
+    
 })
 
 app.listen(8080, function(){
--- utils.js
+++ utils.js
@@ -20,4 +20,31 @@ async function insertRecord(collection, obj)
     }
 }
 
-module.exports = { getDB, insertRecord }
+function getResumeHtml(object){
+    var html = `<!DOCTYPE html><html><body><div style="width:80%; margin-left:10%; margin-right:10%; position:absolute;">`
+    for (item of Object.keys(object)){
+        console.log(object[item])
+        if (object[item]){
+            if (item == "name") html += `<h2 style = 'text-align: center; margin: 0px; font-size: 19px;''>${object[item]}</h2>`
+            if (item == "address" || item == "email" || item == "phone") html += `<p style = 'text-align: center; margin: 0px; font-size: 14px;'>${object[item]}</p>`
+            if (item.includes("section-title"))  html += `<h3 style = 'margin-bottom: 0px; font-size: 17px;'>${object[item]}</h3>
+            <hr style = 'margin: 0px; border:black 1px solid;'></hr>`
+            if (item.includes("item-name")) html += `<span style ='font-weight: bold; margin: 0px; font-size: 15px;'>${object[item]}</span>`
+            if (item.includes("date-start")) html += `<span style ='position: absolute; right: 0px; margin: 0px; font-size: 15px;font-style: italic;'>${object[item]}`
+            if (item.includes("date-end")) html += `- ${object[item]}</span>` 
+            if (item.includes("role")) html += `<p style ='margin: 0px; font-size: 15px;font-style: italic;'>${object[item]}</p>`        
+            if (item.includes("description")){
+                html += "<ul style = 'list-style-position: outside; margin: 0px;'>"
+                for (var row of object[item].split("\n")){
+                    html += `<li style ='margin: 0px; font-size: 15px;font-style;'>${row}</li>` 
+                }
+                html += "</ul>"
+            }
+        }  
+    }
+    html += `</div></body></html>`
+
+    return html
+}
+
+module.exports = { getDB, insertRecord, getResumeHtml}
