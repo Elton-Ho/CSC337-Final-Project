27eef96c01c4d96d52d002831ded75b0b1aa94aa
Zero497 <crscott1@arizona.edu>
2025-05-03 13:40:01 -0700

    prog

--- utils.js
+++ utils.js
@@ -20,4 +20,38 @@ async function insertRecord(collection, obj)
     }
 }
 
-module.exports = { getDB, insertRecord }
+async function fullInsert(client, collectionName, dbName, obj)
+{
+    try{
+        const coll = await getDB(client, collectionName, dbName);
+        await insertRecord(coll, obj)
+        return true;
+    }catch (err)
+    {
+        return false
+    }finally {
+        client.close()
+    }
+}
+
+async function searchByString(client, collectionName, dbName, fieldName, searchStr)
+{
+    try{
+        const coll = await getDB(client, collectionName, dbName);
+        if(searchStr === "")
+        {
+            return await coll.find().toArray()
+        }
+        const query = {
+            [fieldName]: { $regex: searchStr, $options: 'i' }
+        }
+        return await coll.find(query).toArray()
+    }catch (err)
+    {
+        return null
+    }finally {
+        client.close()
+    }
+}
+
+module.exports = { getDB, insertRecord, fullInsert, searchByString }
